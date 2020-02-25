51cc1028374bd7658d02c3333ec144a29b7d2ae2
YunYouJun <me@yunyoujun.cn>
2020-02-26 05:12:58 +0800

    :package: update & simplify

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Vue-Parcel-Demo
+# vue-parcel-demo
 
-小 demo ~
+![GitHub package.json dependency version (dev dep on branch)](https://img.shields.io/github/package-json/dependency-version/YunYouJun/vue-parcel-demo/dev/vue)
+![GitHub package.json dependency version (dev dep on branch)](https://img.shields.io/github/package-json/dependency-version/YunYouJun/vue-parcel-demo/dev/parcel-bundler)
 
-## [Parcel.js + Vue 搭配笔记](https://yunyoujun.cn/note/vue-parcel-demo/)
+本项目为使用 Parcel 打包 Vue 的小 Demo，相比[官网](https://parceljs.org/vue.html) 提供的快速开始，可能更加接近工程（开发小应用）的项目结构。
+
+更多内容请参见 [Parcel.js + Vue 搭配笔记](https://www.yunyoujun.cn/note/vue-parcel-demo/)。
+
+- [我很可爱，请给我钱！](https://github.com/YunYouJun/give-me-money)：另一个使用 Parcel + Vue 实现的小应用
+
+实际上，随着 Parcel 的不断更新和理念的贯彻，使用起来越来越不需要配置。
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,43 +4,28 @@
     <h2>Essential Links</h2>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
+        <a href="https://vuejs.org" target="_blank">
           Vue
         </a>
       </li>
       <li>
-        <a
-          href="https://yunyoujun.cn/note/vue-parcel-demo/"
-          target="_blank"
-        >
+        <a href="https://yunyoujun.cn/note/vue-parcel-demo/" target="_blank">
           Note
         </a>
       </li>
       <li>
-        <a
-          href="https://parceljs.org"
-          target="_blank"
-        >
+        <a href="https://parceljs.org" target="_blank">
           Parcel
         </a>
       </li>
       <li>
-        <a
-          href="https://github.com/YunYouJun"
-          target="_blank"
-        >
+        <a href="https://github.com/YunYouJun" target="_blank">
           @YunYouJun
         </a>
       </li>
-      <br>
+      <br />
       <li>
-        <a
-          href="https://github.com/YunYouJun/vue-parcel-demo"
-          target="_blank"
-        >
+        <a href="https://github.com/YunYouJun/vue-parcel-demo" target="_blank">
           Demo
         </a>
       </li>
@@ -48,34 +33,22 @@
     <h2>Ecosystem</h2>
     <ul>
       <li>
-        <a
-          href="https://github.com/YunYouJun/give-me-money"
-          target="_blank"
-        >
+        <a href="https://github.com/YunYouJun/give-me-money" target="_blank">
           I'm cute!
         </a>
       </li>
       <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://vuex.vuejs.org/" target="_blank">
           vuex
         </a>
       </li>
       <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://vue-loader.vuejs.org/" target="_blank">
           vue-loader
         </a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
           awesome-vue
         </a>
       </li>
@@ -85,18 +58,19 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js + Parcel App'
-    }
+      msg: "Welcome to Your Vue.js + Parcel App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
